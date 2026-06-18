f04526a51b7bed2274c41c00d0b2d4eb69766596
fauziseptians <fauzibelajar96@gmail.com>
2026-06-18 13:23:26 +0700

    fix: issue no no

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 
 export default function Home() {
   return (
-    <div>INI HOME FEATURE fix bug ceritanya pura pura issue aja</div>
+    <div>INI HOME FEATURE fix bug ceritanya pura pura issue aja no o</div>
   );
 }
